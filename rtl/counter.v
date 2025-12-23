e180d73d82628315313bb2de480ee704ee0308a9
Shubhanshu Pokharel <156258477+Sixteen1-6@users.noreply.github.com>
2025-12-23 17:07:47 -0500

    added verilog file

---
+++ rtl/counter.v
@@ -0,0 +1,18 @@
+module counter( // decleares hardware module counter
+    input wire clk // clock signal
+    input wire reset // when 1 go to 0
+    input wire enable // when high count and pause when low
+output reg [3:0]  //4bit
+);
+always @(posedge clk or posedge reset) begin // rising or falling edge
+    if (reset) begin
+        count <= 4'b0000; // reset has priority
+    end else if (enable) begin
+        count <= count +1; // increment counter each clock cycle
+    end
+    // If enable is low count stays paused
+end
+
+endmodule
+
+
